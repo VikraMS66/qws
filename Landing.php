fd31901811635610a53f5dd4a9ef29f7fa0ac22f
Vikram Singh <84076506+VikraMS66@users.noreply.github.com>
2022-07-19 03:14:43 +0530

    Add files via upload

---
+++ Landing.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+    />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+      integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+      crossorigin="anonymous"
+    ></script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+    <title>Home</title>
+</head>
+<body class=' text-white'>
+
+<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-info border border-info mb-5 pt-3 pb-3" style='width:100%;'>
+     <img src='./images/Logo.png'/> <h3 class='text-light'>QUANTITY WATER SUPPLIE</h3>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div 
+        class="collapse navbar-collapse justify-content-end"
+        id="navbarNavAltMarkup"
+      >
+        <ul class="navbar-nav ">
+              <li class="nav-item">
+                    <a id="book" style='font-weight:600' class="nav-link  h2 btn btn-light btn-lg text-dark rounded-pill pl-5 pr-5" href="./Customer/signup.php" name="book" title="Booking Website">Book Now</a>
+             </li>
+         </ul>
+      </div>
+    </nav>
+
+    <section class='text-center pb-4 ' id='Quote' style="background-image: url('./images/home.jpg'); width:100%; height:500px; padding-top:160px;">
+            <h1 class='pb-5'><strong>“Thousands have lived without love, not one without water.”</strong><br> -H. Auden.</h1>
+            <h1><strong>“Water is life's matter and matrix, mother and medium. There is no life without water.”</strong><br> – Albert Szent-Gyorgyi, M.D. Discoverer of Vitamin C.</h1>
+</section>
+
+
+<section id='Ordering' class='' style="width:100%; height:600px; ">
+    
+            <img class='pt-5 ' src='./images/process.png'> 
+           
+  
+</section>
+
+    <section id='saveWater' class='text-dark mx-0' style="background-image: url('./images/save.png'); width:100%; height:750px; ">
+                        
+       <div class='row'>
+         <div class='col-2'>
+         </div>
+         <div class='col-7 pt-5 pb-3'>
+       
+                <div id="carouselExampleControls" style=" height:300px;" class="carousel slide" data-ride="carousel">
+
+                 <div class="carousel-inner">
+                    <div class="carousel-item active" >
+                    <img class="d-block w-100" src="./images/save1.jpg" alt="First slide">
+                </div>
+
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="./images/save2.jpg" alt="Second slide">
+                </div>
+
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="./images/save3.jpg" alt="Third slide">
+                </div>
+
+
+                </div>
+
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+
+        </div>
+       
+
+      
+          
+            </div> 
+
+</section>
+
+
+
+    <section id='features' style="width:100%; height:500px; ">
+
+    <img class='' src='./images/benefits.png'> 
+           
+          
+</section>
+
+<section id='contact' style="width:100%; height:300px; ">
+     <img class='' src='./images/contact.png'> 
+</section>
+
+
+<div style=''>
+  <section id='footer' class='pl-3' style=" width:100%; height:250px; padding-top:160px; ">
+  <img class='' src='./images/footer.png'> 
+   </section>
+</div>
+</body>
+</html>
